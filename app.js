4244d52a90f799f81d193252dd156a8fe260aa52
MagiasSamuel074 <cervantessamuel074@hotmail.com>
2025-12-08 20:51:34 -0600

    un poco de avance

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-let numero1 = 1;
+class Calculadora{
+    static Suma(){
+        let NumerosPorSumar = parseInt(prompt("cuantos numeros deseas sumar: "));
+        for(let i = 0; i <NumerosPorSumar; i++){
+            let numero = parseInt(prompt("numero a sumar: "));
+            pass 
+        }
+    }
+}
